49e835669f425f9b43921fd7baf97865efee6268
RickyRaMx_c149 <ricardo.rmeza@live.com>
2024-03-24 07:15:11 +0000

    cambios en servicios

--- src/pages/2.servicios/servicios.jsx
+++ src/pages/2.servicios/servicios.jsx
@@ -1,28 +1,32 @@
 import Contacto from '../../components/contacto/contacto';
+import Cards from '../../components/cards/cards';
+
 
 const servicios = () => {
   return (
     <>
     {/* HERO INCIO */}
-    <div className="grid md:grid-cols-2 py-6 items-center md:gap-4 gap-8 font-[sans-serif] text-[#333] max-w-5xl max-md:max-w-md mx-auto">
-      <div className="max-md:order-1 max-md:text-center">
-        <h3 className="md:text-3xl text-2xl md:leading-10">Esto es lo que hacemos
- y lo hacemos muy bien.</h3>
-        <p className="mt-4 text-sm">Tenemos nuestra propia historia y experiencias compuestas por 
-personas y elementos que hoy constituyen un todo.
- Tenemos nuestra propia historia y experiencias compuestas por 
-personas y elementos que hoy constituyen un todo.Tenemos 
-nuestra propia historia y experiencias compuestas por personas 
-y elementos que hoy constituyen un todo.Tenemos nuestra 
-propia historia y experiencias compuestas por personas y ele
-mentos que hoy constituyen un todo.</p>
-        <button type="button" className="px-6 py-2 mt-8 font-semibold rounded text-sm outline-none border-2 border-[#333] hover:bg-[#333] hover:text-white transition-all duration-300">Explore</button>
+    <div className="grid-servicio">
+      <div className="servicios--contenedor-uno">
+        <h2 className='text-left mt-6 text-2xl font-bold pt-24'>Servicio</h2>
+        <h3 className='text-left mt-2 pt-3 text-7xl font-bold text-[#425cc7]'>Esto es lo que hacemos <br /> y lo hacemos muy bien.</h3>
+        <div className="servicios-circulo-dos">
+        <p className="text-left mt-3 pt-3 w-2/5">Tenemos nuestra propia historia y experiencias compuestas por 
+          personas y elementos que hoy constituyen un todo.
+          Tenemos nuestra propia historia y experiencias compuestas por 
+          personas y elementos que hoy constituyen un todo.Tenemos 
+          nuestra propia historia y experiencias compuestas por personas 
+          y elementos que hoy constituyen un todo.Tenemos nuestra 
+          propia historia y experiencias compuestas por personas y ele
+          mentos que hoy constituyen un todo.</p>
+        </div>
       </div>
-      <div className="md:h-[450px]">
-        <img src="https://readymadeui.com/photo.webp" className="w-full h-full md:object-contain" />
+      <div className="servicios--contenedor-circulos mb-14">
       </div>
     </div>
+    
     {/* HERO FIN */}
+    <Cards />
     <Contacto />
     </>
   )
