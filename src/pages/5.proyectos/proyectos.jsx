70a3e9ba8f9ee9a4ca3893c69824b3f0bf06b9a9
RickyRaMx_c149 <ricardo.rmeza@live.com>
2024-03-25 01:34:40 +0000

    proyectos, acento

--- src/pages/5.proyectos/proyectos.jsx
+++ src/pages/5.proyectos/proyectos.jsx
@@ -1,54 +1,46 @@
+import { Link } from 'react-router-dom';
 import React from 'react'
+import Imagen from '../../assets/pixel-art-7284052_1280.png'
 
 export default function proyectos() {
   return (
     <>
-    <div className="grid md:grid-cols-2 items-center py-6 md:gap-8 gap-6 font-[sans-serif] text-[#333] max-w-5xl max-md:max-w-md mx-auto">
-      <div className="max-md:order-1 max-md:text-center">
-        <h2 className="md:text-4xl text-3xl md:leading-10 font-extrabold text-[#333] mb-4">Savor Every Bite</h2>
-        <p className="mt-4 text-base text-[#666] leading-relaxed">Embark on a gastronomic journey with our curated dishes, delivered promptly to your doorstep. Elevate your dining experience today.</p>
-        <div className="mt-8 flex max-sm:flex-col sm:space-x-4 max-sm:space-y-6">
-          <a href="javascript:void(0);" className="px-6 py-3 text-base font-semibold text-white bg-[#f032e6] rounded-full hover:bg-opacity-80 transition-all duration-300 transform hover:scale-105 focus:ring-2 focus:ring-[#f032e6] focus:outline-none focus:ring-opacity-50">Explore Our Menu</a>
-          <a href="javascript:void(0);" className="px-6 py-3 text-base font-semibold text-[#f032e6] border border-[#f032e6] rounded-full hover:text-white hover:bg-[#f032e6] transition-all duration-300 transform hover:scale-105 focus:ring-2 focus:ring-[#f032e6] focus:outline-none focus:ring-opacity-50">Order Now</a>
+    <main className='pt-14'>
+      <div className="contenedor-proyectos ">
+        <div className="proyecto-cuadro">
+          <h1 className='text-left pt-2 text-2xl font-extrabold'>Nuestros proyectos</h1>
+          <h2 className='text-left text-7xl pt-2 font-bold text-[#425cc7]'>Cada proyecto <br /> es unico</h2>
+        </div>
+        <div className="proyecto-texto">
+          <p className='w-2/5 text-left pt-2'>Tenemos nuestra propia historia y experiencias compuestas por
+            personas y elementos que hoy constituyen un todo.
+            Tenemos nuestra propia historia y experiencias compuestas por</p>
         </div>
       </div>
-      <div className="md:h-[450px]">
-        <img src="https://readymadeui.com/hotel-img.webp" className="w-full h-full object-cover rounded-lg shadow-xl" alt="Dining Experience" />
+    </main>
+    {/* grid inicio de imagenes */}
+    <div className="proyectos-imgs-grid mt-8">
+      <div className="proyecto-img-uno">
+        <Link to={'/cliente/cliente-acento-coffe-roasters'}>
+          <img src={Imagen} alt="" className='tienda'/>
+        </Link>
+      </div>
+      <div className="proyecto-imgenes-dos">
+      <img src={Imagen} alt="" />
+      <img src={Imagen} alt="" />
       </div>
     </div>
+    {/* grid cuatro inicio  */}
+    <div className="proyecto-cuadricula mt-7 mb-24">
+      <img src={Imagen} alt="" />
+      <img src={Imagen} alt="" />
+      <img src={Imagen} alt="" />
+      <img src={Imagen} alt="" />
+    </div>
+    {/* grid cuatro fin  */}
 
     {/* INICIO DE GRID */}
-    <div className="bg-gray-100 md:px-10 px-4 py-12 font-[sans-serif]">
-      <div className="max-md:max-w-lg mx-auto">
-        <h2 className="text-4xl font-extrabold text-gray-800 mb-10">Latest Blog Posts</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="bg-gradient-to-b from-indigo-800 to-indigo-600 text-white rounded-lg overflow-hidden shadow-lg transform hover:scale-105 transition-transform duration-300">
-            <img src="https://readymadeui.com/cardImg.webp" alt="Blog Post 1" className="w-full h-64 object-cover" />
-            <div className="p-6">
-              <h3 className="text-2xl font-semibold mb-2">Lorem Ipsum Dolor</h3>
-              <p className="text-sm opacity-75">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...</p>
-              <a href="javascript:void(0);" className="mt-4 inline-block text-blue-200 text-sm hover:underline">Read More</a>
-            </div>
-          </div>
-          <div className="bg-gradient-to-b from-purple-800 to-purple-600 text-white rounded-lg overflow-hidden shadow-lg transform hover:scale-105 transition-transform duration-300">
-            <img src="https://readymadeui.com/hotel-img.webp" alt="Blog Post 2" className="w-full h-64 object-cover" />
-            <div className="p-6">
-              <h3 className="text-2xl font-semibold mb-2">Consectetur Adipiscing</h3>
-              <p className="text-sm opacity-75">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...</p>
-              <a href="javascript:void(0);" className="mt-4 inline-block text-pink-200 text-sm hover:underline">Read More</a>
-            </div>
-          </div>
-          <div className="bg-gradient-to-b from-teal-800 to-teal-600 text-white rounded-lg overflow-hidden shadow-lg transform hover:scale-105 transition-transform duration-300">
-            <img src="https://readymadeui.com/team-image.webp" alt="Blog Post 3" className="w-full h-64 object-cover" />
-            <div className="p-6">
-              <h3 className="text-2xl font-semibold mb-2">Lorem Ipsum Sit Amet</h3>
-              <p className="text-sm opacity-75">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...</p>
-              <a href="javascript:void(0);" className="mt-4 inline-block text-green-200 text-sm hover:underline">Read More</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    
     </>
   )
 }
--- src/pages/6.proyecto/cliente-acento-coffe-roaster.jsx
+++ src/pages/6.proyecto/cliente-acento-coffe-roaster.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import Contacto from '../../components/contacto/contacto';
+import Imagen from '../../assets/pixel-art-7284052_1280.png'
 
 export default function AcentoCoffeRoaster() {
   return (
-    <div>AienteCcentRcoffe-roaster</div>
+    <>
+      <main className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-12">
+            <div className="p-12 md:w-1/2 flex flex-col items-start">
+              {/* <span className="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">CATEGORY</span> */}
+              <h2 className="sm:text-3xl text-7xl title-font font-bold text-[#425cc7] mt-4 mb-4">Acento Coffe Roasters</h2>
+              <p className="leading-relaxed mb-8 text-left">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric. Cray pug you probably haven't heard of them hexagon kickstarter craft beer pork chic.</p>
+              
+            </div>
+            <div className="p-12 md:w-1/2 flex flex-col items-start">
+              
+            </div>
+          </div>
+        </div>
+      </main>
+      <section>
+        <div className="cafe-imagen-uno">
+          <img src={Imagen} alt="" className='' />
+        </div>
+        <div className="cafe-imagen_grid mt-7 mb-7">
+        <img src={Imagen} alt="" className='' />
+        <img src={Imagen} alt="" className='' />
+        </div>
+        <div className="cafe-imagen-uno">
+          <img src={Imagen} alt="" className='' />
+        </div>
+      </section>
+      <Contacto />
+    </>
   )
 }
