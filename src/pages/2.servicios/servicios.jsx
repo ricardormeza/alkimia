56eee7c63a35d5549703e249966b4ac3cc4959a7
ricardormeza <ricardo.rmeza@live.com>
2024-04-01 16:32:24 -0700

    cambios en css en servicios

--- src/pages/2.servicios/servicios.jsx
+++ src/pages/2.servicios/servicios.jsx
@@ -6,12 +6,12 @@ const servicios = () => {
   return (
     <>
     {/* HERO INCIO */}
-    <div className="grid-servicio">
+    <div className="contenedor grid-servicio md:pb-40 md:py-16">
       <div className="servicios--contenedor-uno">
-        <h2 id='servicios' className='text-left mt-6 text-2xl font-bold pt-24'>Servicio</h2>
-        <h3 className='text-left mt-2 pt-3 text-7xl font-bold text-[#425cc7]'>Esto es lo que hacemos <br /> y lo hacemos muy bien.</h3>
+        <h2 id='servicios' className='text-left text-2xl font-bold pt-24'>Servicio</h2>
+        <h3 className='text-left mt-2  text-7xl font-bold text-[#425cc7]'>Esto es lo que hacemos <br className='' /> y lo hacemos muy bien.</h3>
         <div className="servicios-circulo-dos">
-        <p className="text-left mt-3 pt-3 w-2/5">Tenemos nuestra propia historia y experiencias compuestas por 
+        <p className="text-left mt-3 pt-3 w-2/5 ">Tenemos nuestra propia historia y experiencias compuestas por 
           personas y elementos que hoy constituyen un todo.
           Tenemos nuestra propia historia y experiencias compuestas por 
           personas y elementos que hoy constituyen un todo.Tenemos 
