f821365ef3a565a149bbefe9018938a73502fded
ricardormeza <ricardo.rmeza@live.com>
2024-04-01 17:09:12 -0700

    css proyecto pagina general de proyectos

--- src/pages/5.proyectos/proyectos.jsx
+++ src/pages/5.proyectos/proyectos.jsx
@@ -5,14 +5,14 @@ import Imagen from '../../assets/pixel-art-7284052_1280.png'
 export default function proyectos() {
   return (
     <>
-    <main className='pt-14'>
-      <div className="contenedor-proyectos ">
+    <main className='pt-14 pb-20'>
+      <div className="contenedor contenedor-proyectos">
         <div className="proyecto-cuadro">
           <h1 className='text-left pt-2 text-2xl font-extrabold'>Nuestros proyectos</h1>
           <h2 className='text-left text-7xl pt-2 font-bold text-[#425cc7]'>Cada proyecto <br /> es unico</h2>
         </div>
         <div className="proyecto-texto">
-          <p className='w-2/5 text-left pt-2'>Tenemos nuestra propia historia y experiencias compuestas por
+          <p className='sm:w-4/5 md:w-2/5 text-left pt-2'>Tenemos nuestra propia historia y experiencias compuestas por
             personas y elementos que hoy constituyen un todo.
             Tenemos nuestra propia historia y experiencias compuestas por</p>
         </div>
